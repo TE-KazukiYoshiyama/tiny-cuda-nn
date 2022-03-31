b9a6d6568b7776177ba67fd042f6f53bc925b0a7
Kazuki.Yoshiyama <Kazuki.Yoshiyama@sony.com>
2022-03-31 16:05:22 +0900

    Fix grad input issue temporally and add inference option

--- benchmarks/mlp/tcnn_linear.py
+++ benchmarks/mlp/tcnn_linear.py
@@ -13,11 +13,11 @@ def main(args):
     print(f"Tiny Cuda NN")
     for n_features in args.n_features:
         for n_layers in args.n_layers:
-            bench(args.device_id, n_features, n_layers, args.forward_only)
+            bench(args.device_id, n_features, n_layers, args.forward_only, args.inference_only)
 
 
-def bench(device_id, n_features, n_layers, forward_only):
-    B, R, N, D = 4, 512, 256, 3
+def bench(device_id, n_features, n_layers, forward_only, inference_only):
+    B, R, N, D = 4, 512, 256, n_features
     device = torch.device(f"cuda:{device_id}")
     n_iters = 100
 
@@ -35,7 +35,7 @@ def bench(device_id, n_features, n_layers, forward_only):
     "network": {
         "otype": "FullyFusedMLP",
         "activation": "ReLU",
-        "output_activation": "None",
+        "output_activation": "ReLU",
         "n_neurons": ${n_features},
         "n_hidden_layers": ${n_layers}
     }
@@ -54,16 +54,22 @@ def bench(device_id, n_features, n_layers, forward_only):
     ## )
 
     ## encoding = tcnn.Encoding(n_input_dims, config["encoding"])
-    model = tcnn.Network(3, n_features, config["network"])
 
-    x = torch.randn(B*R*N, D, dtype=torch.float32, requires_grad=True).to(device)
-    
+    D_input = n_features
+    model = tcnn.Network(D_input, n_features, config["network"])
+
+    # the grad computation wrt the first input is very slow..., so set as False
+    x = torch.randn(B*R*N, D_input, dtype=torch.float16, requires_grad=False).to(device)
+    dy = torch.randn(B*R*N, n_features, dtype=torch.float16).to(device)
     def evaluate():
         for i in range(n_iters):
-            y = model(x)
-            y = torch.sum(y)
-            if not forward_only:
-                y.backward()
+            if inference_only:
+                with torch.no_grad():
+                    y = model(x)
+            else:
+                y = model(x)
+            if not (forward_only or inference_only):
+                y.backward(dy)
         y.cpu()
         
     evaluate()
@@ -79,6 +85,7 @@ if __name__ == '__main__':
     parser.add_argument("--n_layers", type=int, nargs="+", default=[12, 24, 36])
     parser.add_argument("--n_features", type=int, nargs="+", default=[32, 64, 128])
     parser.add_argument("--forward_only", action="store_true")
+    parser.add_argument("--inference_only", action="store_true")
     
     args = parser.parse_args()
     
